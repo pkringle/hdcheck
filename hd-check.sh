24a1fed3a597b246f37f68a602f500a5e4e6e404
Peter Kringle <pkringle@g2llc.com>
2015-05-20 15:26:27 -0700

    Adding append so that we capture all the data per run

--- hd-check.sh
+++ hd-check.sh
@@ -4,6 +4,6 @@ clear
 smartctl -t short /dev/$1
 badblocks -b 4096 -wsv /dev/$1 | tee $1_results.txt
 sleep 60
-smartctl -l selftest /dev/$1 | tee $1_results.txt
-smartctl -d ata -i /dev/$1 | tee $1_results.txt
+smartctl -l selftest /dev/$1 | tee -a $1_results.txt
+smartctl -d ata -i /dev/$1 | tee -a $1_results.txt
 
