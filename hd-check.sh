c12666e8b4e519f092c1755c4c8c193527f393aa
Peter Kringle <pkringle@g2llc.com>
2015-05-20 15:21:06 -0700

    writing results to local log file also just incase the screen gets messed up

--- hd-check.sh
+++ hd-check.sh
@@ -2,8 +2,8 @@
 
 clear
 smartctl -t short /dev/$1
-badblocks -b 4096 -wsv /dev/$1
+badblocks -b 4096 -wsv /dev/$1 | tee $1_results.txt
 sleep 60
-smartctl -l selftest /dev/$1
-smartctl -d ata -i /dev/$1
+smartctl -l selftest /dev/$1 | tee $1_results.txt
+smartctl -d ata -i /dev/$1 | tee $1_results.txt
 
