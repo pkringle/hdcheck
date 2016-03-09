b1741a96bea60cd8b73c0d687edbe19db1b255f1
Peter Kringle <pkringle@g2llc.com>
2016-03-08 16:19:23 -0800

    Adding zeros to disks at the end

--- hd-check.sh
+++ hd-check.sh
@@ -3,6 +3,7 @@
 clear
 smartctl -t short /dev/$1
 badblocks -b 4096 -wsv /dev/$1 | tee $1_results.txt
+dd if=/dev/zero /dev/$1 iflag=nocache oflag=direct bs=4096
 sleep 60
 smartctl -l selftest /dev/$1 | tee -a $1_results.txt
 smartctl -d ata -i /dev/$1 | tee -a $1_results.txt
